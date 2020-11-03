ff7a5199ae492d21395f4fdec6c625884a5750f1
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2020-11-03 21:56:49 +0600

    external function for various tests

--- system-check.sh
+++ system-check.sh
@@ -168,6 +168,59 @@ chk_SvcExist() {
     fi
 }
 
+cpu_info() {
+	echo -en "Model name:\t\t${green}$(lscpu | grep -oP 'Model name:\s*\K.+')${nc}\n"
+	echo -en "Vendor ID:\t\t${green}$(lscpu | grep -oP 'Vendor ID:\s*\K.+')${nc}\n"
+	Info "CPU Cores\t\t" `awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo`
+	Info "CPU MHz:\t\t" `lscpu | grep -oP 'CPU MHz:\s*\K.+'`
+	Info "Hypervisor vendor:\t" `lscpu | grep -oP 'Hypervisor vendor:\s*\K.+'`
+	Info "Virtualization:\t\t" `lscpu | grep -oP 'Virtualization:\s*\K.+'`
+	Info "CPU Usage:\t\t" `cat /proc/stat | awk '/cpu/{printf("%.2f%\n"), ($2+$4)*100/($2+$4+$5)}' |  awk '{print $0}' | head -1`
+}
+
+test_disk() {
+
+	if [[ "$SKIPDISK" -eq "1" ]]; then
+		Info "" "Disk test was skipped"
+	else
+		echo -en "Write (1st):\t\t${green}$(dd if=/dev/zero of=$TESTFILE bs=1M count=1024 |& awk '/copied/ {print $0}' | sed 's:.*,::')${nc}\n"
+		echo -en "Write (2nd):\t\t${green}$(dd if=/dev/zero of=$TESTFILE bs=1M count=1024 |& awk '/copied/ {print $0}' | sed 's:.*,::')${nc}\n"
+		echo -en "Write (3nd):\t\t${green}$(dd if=/dev/zero of=$TESTFILE bs=1M count=1024 |& awk '/copied/ {print $0}' | sed 's:.*,::')${nc}\n"
+		echo ""
+		echo -en "Read (1st):\t\t${green}$(dd if=$TESTFILE of=/dev/null bs=1M count=1024 |& awk '/copied/ {print $0}' | sed 's:.*,::')${nc}\n"
+		echo -en "Read (2nd):\t\t${green}$(dd if=$TESTFILE of=/dev/null bs=1M count=1024 |& awk '/copied/ {print $0}' | sed 's:.*,::')${nc}\n"
+		echo -en "Read (3nd):\t\t${green}$(dd if=$TESTFILE of=/dev/null bs=1M count=1024 |& awk '/copied/ {print $0}' | sed 's:.*,::')${nc}\n"
+		rm -rf $TESTFILE
+	fi
+
+}
+
+disk_usage() {
+	echo -e "( 0-90% = OK/HEALTHY, 90-95% = WARNING, 95-100% = CRITICAL )"
+	echo -e "$d$d"
+	echo -e "Mounted File System[s] Utilization (Percentage Used):\n"
+
+	COL1=$(echo "$FS_USAGE"|awk '{print $1 " "$7}')
+	COL2=$(echo "$FS_USAGE"|awk '{print $6}'|sed -e 's/%//g')
+
+	for i in $(echo "$COL2"); do
+	{
+	  if [ $i -ge 95 ]; then
+	    COL3="$(echo -e $i"% $CriticalMark\n$COL3")"
+	  elif [[ $i -ge 90 && $i -lt 95 ]]; then
+	    COL3="$(echo -e $i"% $WarningMark\n$COL3")"
+	  else
+	    COL3="$(echo -e $i"% $SuccessMark\n$COL3")"
+	  fi
+	}
+	done
+	COL3=$(echo "$COL3"|sort -k1n)
+	paste  <(echo "$COL1") <(echo "$COL3") -d' '|column -t
+
+	# https://unix.stackexchange.com/questions/43875/sending-the-output-from-dd-to-awk-sed-grep
+	# https://www.shellhacks.com/disk-speed-test-read-write-hdd-ssd-perfomance-linux/
+}
+
 speedtest_v4() {
 	local res=$(wget -4O /dev/null -T200 $1 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}')
 	local region=$2
@@ -184,6 +237,41 @@ test_v4() {
 		speedtest_v4 "http://speedtest.sjc01.softlayer.com/downloads/test10.zip" "San Jose, California (west)"
 		speedtest_v4 "http://speedtest.frankfurt.linode.com/100MB-frankfurt.bin" "Frankfurt, DE, JP\t"
 	fi
+}
+
+system_info() {
+	checkDistro
+	Info "Hostname:\t\t" $HOSTNAME
+	Info "Distro:\t\t\t" "${DISTRO}"
+	Info "IP:\t\t\t" $SERVER_IP
+	Info "External IP:\t\t" $(curl -s ifconfig.co)
+
+	isRoot
+	isSELinux
+
+	Info "Kernel:\t\t\t" `uname -r`
+	Info "Architecture:\t\t" `arch`
+	Info "Active User:\t\t" `w | cut -d ' ' -f1 | grep -v USER | xargs -n1`
+	echo -en "Current Load Average:\t${green}$(uptime|grep -o "load average.*"|awk '{print $3" " $4" " $5}')${nc}"
+}
+
+mem_info() {
+	Info "Total memory:\t\t" "${TOTALMEM}Mb"
+	Info "Memory Usage:\t\t" `free | awk '/Mem/{printf("%.2f%"), $3/$2*100}'`
+	space
+	if free | awk '/^Swap:/ {exit !$2}'; then
+		TOTALSWAP=$(free -m | awk '$1=="Swap:" {print $2}')
+		Info "Total swap:\t\t" "${TOTALSWAP}Mb"
+	    Info "Swap Usage:\t\t" `free | awk '/Swap/{printf("%.2f%"), $3/$2*100}'`
+	else
+	    Error "Swap Usage:\t\t" "swap does not exist"
+	fi
+}
+
+boot_info() {
+	Info "Active User:\t\t" `w | cut -d ' ' -f1 | grep -v USER | xargs -n1`
+	echo -en "Last Reboot:\t\t${green}$(who -b | awk '{print $3,$4,$5}')${nc}"
+	echo -en "\nUptime:\t\t\t${green}`awk '{a=$1/86400;b=($1%86400)/3600;c=($1%3600)/60} {printf("%d days, %d hour %d min\n",a,b,c)}' /proc/uptime`${nc}"
 }
 
 confirm() {
@@ -203,46 +291,20 @@ confirm() {
 # ---------------------------------------------------\
 space
 Splash "-------------------------------\t\tSystem Information\t----------------------------"
-checkDistro
-Info "Hostname:\t\t" $HOSTNAME
-Info "Distro:\t\t\t" "${DISTRO}"
-Info "IP:\t\t\t" $SERVER_IP
-Info "External IP:\t\t" $(curl -s ifconfig.co)
 
-isRoot
-isSELinux
-
-Info "Kernel:\t\t\t" `uname -r`
-Info "Architecture:\t\t" `arch`
-Info "Active User:\t\t" `w | cut -d ' ' -f1 | grep -v USER | xargs -n1`
-echo -en "Current Load Average:\t${green}$(uptime|grep -o "load average.*"|awk '{print $3" " $4" " $5}')${nc}"
+system_info
 
 Splash "\n\n-------------------------------\t\tCPU Information\t\t------------------------------"
-echo -en "Model name:\t\t${green}$(lscpu | grep -oP 'Model name:\s*\K.+')${nc}\n"
-echo -en "Vendor ID:\t\t${green}$(lscpu | grep -oP 'Vendor ID:\s*\K.+')${nc}\n"
-Info "CPU Cores\t\t" `awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo`
-Info "CPU MHz:\t\t" `lscpu | grep -oP 'CPU MHz:\s*\K.+'`
-Info "Hypervisor vendor:\t" `lscpu | grep -oP 'Hypervisor vendor:\s*\K.+'`
-Info "Virtualization:\t\t" `lscpu | grep -oP 'Virtualization:\s*\K.+'`
-Info "CPU Usage:\t\t" `cat /proc/stat | awk '/cpu/{printf("%.2f%\n"), ($2+$4)*100/($2+$4+$5)}' |  awk '{print $0}' | head -1`
+
+cpu_info
 
 Splash "\n\n-------------------------------\t\tMemory Information\t\t------------------------------"
 
-Info "Total memory:\t\t" "${TOTALMEM}Mb"
-Info "Memory Usage:\t\t" `free | awk '/Mem/{printf("%.2f%"), $3/$2*100}'`
-space
-if free | awk '/^Swap:/ {exit !$2}'; then
-	TOTALSWAP=$(free -m | awk '$1=="Swap:" {print $2}')
-	Info "Total swap:\t\t" "${TOTALSWAP}Mb"
-    Info "Swap Usage:\t\t" `free | awk '/Swap/{printf("%.2f%"), $3/$2*100}'`
-else
-    Error "Swap Usage:\t\t" "swap does not exist"
-fi
+mem_info
 
 Splash "\n\n-------------------------------\t\tBoot Information\t------------------------------"
-Info "Active User:\t\t" `w | cut -d ' ' -f1 | grep -v USER | xargs -n1`
-echo -en "Last Reboot:\t\t${green}$(who -b | awk '{print $3,$4,$5}')${nc}"
-echo -en "\nUptime:\t\t\t${green}`awk '{a=$1/86400;b=($1%86400)/3600;c=($1%3600)/60} {printf("%d days, %d hour %d min\n",a,b,c)}' /proc/uptime`${nc}"
+
+boot_info
 
 Splash "\n\n-------------------------------\t\tLast 3 Reboot Info\t------------------------------"
 last reboot | head -3
@@ -254,43 +316,12 @@ Splash "\n\n-------------------------------\t\tMount Information\t--------------
 echo -en "$MOUNT"|column -t
 
 Splash "\n\n-------------------------------\t\tDisk usage\t\t------------------------------"
-echo -e "( 0-90% = OK/HEALTHY, 90-95% = WARNING, 95-100% = CRITICAL )"
-echo -e "$d$d"
-echo -e "Mounted File System[s] Utilization (Percentage Used):\n"
 
-COL1=$(echo "$FS_USAGE"|awk '{print $1 " "$7}')
-COL2=$(echo "$FS_USAGE"|awk '{print $6}'|sed -e 's/%//g')
+disk_usage
 
-for i in $(echo "$COL2"); do
-{
-  if [ $i -ge 95 ]; then
-    COL3="$(echo -e $i"% $CriticalMark\n$COL3")"
-  elif [[ $i -ge 90 && $i -lt 95 ]]; then
-    COL3="$(echo -e $i"% $WarningMark\n$COL3")"
-  else
-    COL3="$(echo -e $i"% $SuccessMark\n$COL3")"
-  fi
-}
-done
-COL3=$(echo "$COL3"|sort -k1n)
-paste  <(echo "$COL1") <(echo "$COL3") -d' '|column -t
-
-# https://unix.stackexchange.com/questions/43875/sending-the-output-from-dd-to-awk-sed-grep
-# https://www.shellhacks.com/disk-speed-test-read-write-hdd-ssd-perfomance-linux/
 Splash "\n\n-------------------------------\t\tTest disk IO\t------------------------------"
 
-if [[ "$SKIPDISK" -eq "1" ]]; then
-		Info "" "Disk test was skipped"
-else
-	echo -en "Write (1st):\t\t${green}$(dd if=/dev/zero of=$TESTFILE bs=1M count=1024 |& awk '/copied/ {print $0}' | sed 's:.*,::')${nc}\n"
-	echo -en "Write (2nd):\t\t${green}$(dd if=/dev/zero of=$TESTFILE bs=1M count=1024 |& awk '/copied/ {print $0}' | sed 's:.*,::')${nc}\n"
-	echo -en "Write (3nd):\t\t${green}$(dd if=/dev/zero of=$TESTFILE bs=1M count=1024 |& awk '/copied/ {print $0}' | sed 's:.*,::')${nc}\n"
-	echo ""
-	echo -en "Read (1st):\t\t${green}$(dd if=$TESTFILE of=/dev/null bs=1M count=1024 |& awk '/copied/ {print $0}' | sed 's:.*,::')${nc}\n"
-	echo -en "Read (2nd):\t\t${green}$(dd if=$TESTFILE of=/dev/null bs=1M count=1024 |& awk '/copied/ {print $0}' | sed 's:.*,::')${nc}\n"
-	echo -en "Read (3nd):\t\t${green}$(dd if=$TESTFILE of=/dev/null bs=1M count=1024 |& awk '/copied/ {print $0}' | sed 's:.*,::')${nc}\n"
-	rm -rf $TESTFILE
-fi
+test_disk
 
 Splash "\n\n-------------------------------\t\tRead-only mounted\t------------------------------"
 echo "$MOUNT"|grep -w \(ro\) && Info "\n.....Read Only file system[s] found"|| Info "No read-only file system[s] found. "
